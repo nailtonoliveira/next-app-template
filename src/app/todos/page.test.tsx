7c078604cdd3483560cc5fe22740af882cfc93b5
Nailton Oliveira <nailton.me91@gmail.com>
2024-07-17 13:32:40 -0300

    test: should delete item from the list correctly

--- src/app/todos/page.test.tsx
+++ src/app/todos/page.test.tsx
@@ -9,6 +9,18 @@ import {
 
 import TodoPage from "./page";
 
+const inputTodoItem = (todoText: string) =>
+  act(() => {
+    fireEvent.change(screen.getByRole("textbox"), {
+      target: { value: todoText },
+    });
+  });
+
+const clickAddButton = () =>
+  act(() => {
+    fireEvent.click(screen.getByRole("button", { name: /add/i }));
+  });
+
 afterEach(() => cleanup());
 
 test("TodoPage should render empty message correctly", () => {
@@ -22,15 +34,9 @@ test("TodoPage should add todo items correctly on the list", async () => {
 
   render(<TodoPage />);
 
-  act(() => {
-    fireEvent.change(screen.getByRole("textbox"), {
-      target: { value: addTodoText },
-    });
-  });
+  inputTodoItem(addTodoText);
 
-  act(() => {
-    fireEvent.click(screen.getByRole("button", { name: /add/i }));
-  });
+  clickAddButton();
 
   expect(screen.getByText(addTodoText)).toBeDefined();
 });
@@ -38,9 +44,7 @@ test("TodoPage should add todo items correctly on the list", async () => {
 test("TodoPage should not add empty todo items on the list", async () => {
   render(<TodoPage />);
 
-  act(() => {
-    fireEvent.click(screen.getByRole("button", { name: /add/i }));
-  });
+  clickAddButton();
 
   expect(screen.getByTestId("todo-list").childElementCount).toBe(0);
 });
@@ -48,16 +52,45 @@ test("TodoPage should not add empty todo items on the list", async () => {
 test("TodoPage should clear field after add a todo item on the list", async () => {
   render(<TodoPage />);
 
-  act(() => {
-    fireEvent.change(screen.getByRole("textbox"), {
-      target: { value: "Todo item" },
-    });
-  });
+  inputTodoItem("Todo item");
 
-  act(() => {
-    fireEvent.click(screen.getByRole("button", { name: /add/i }));
-  });
+  clickAddButton();
 
   expect(screen.getByText("Todo item")).toBeDefined();
   expect(screen.getByRole("textbox")).toHaveValue("");
+});
+
+test("TodoPage should avoid adding repeated items on the list", async () => {
+  const todoItemText = "Todo item";
+
+  render(<TodoPage />);
+
+  inputTodoItem(todoItemText);
+  clickAddButton();
+
+  inputTodoItem(todoItemText);
+  clickAddButton();
+
+  expect(screen.getAllByText(todoItemText)).toHaveLength(1);
+});
+
+test("TodoPage should delete item from the list correctly", async () => {
+  const todoItemText = "Todo item";
+
+  render(<TodoPage />);
+
+  inputTodoItem(todoItemText);
+  clickAddButton();
+
+  expect(screen.getByText(todoItemText)).toBeInTheDocument();
+
+  act(() => {
+    fireEvent.click(
+      screen.getByRole("button", {
+        name: /delete ❌/i,
+      })
+    );
+  });
+
+  expect(screen.queryByText(todoItemText)).not.toBeInTheDocument();
 });
