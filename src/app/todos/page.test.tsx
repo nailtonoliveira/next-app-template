137f0e4ba59d78f5b54a9199e09ff6f6f4b81d28
Nailton Oliveira <nailton.me91@gmail.com>
2024-07-17 00:04:51 -0300

    test: should add todo items correctly on the list

--- src/app/todos/page.test.tsx
+++ src/app/todos/page.test.tsx
@@ -1,10 +1,36 @@
-import { expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { afterEach, expect, test } from "vitest";
+import {
+  act,
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+} from "@testing-library/react";
 
 import TodoPage from "./page";
+
+afterEach(() => cleanup());
 
 test("TodoPage should render empty message correctly", () => {
   render(<TodoPage />);
 
   expect(screen.getByText(/no tasks todo/i)).toBeDefined();
+});
+
+test("TodoPage should add todo items correctly on the list", async () => {
+  const addTodoText = "Make a test with this component";
+
+  render(<TodoPage />);
+
+  act(() => {
+    fireEvent.change(screen.getByRole("textbox"), {
+      target: { value: addTodoText },
+    });
+  });
+
+  act(() => {
+    fireEvent.click(screen.getByRole("button", { name: /add/i }));
+  });
+
+  expect(screen.getByText(addTodoText)).toBeDefined();
 });
